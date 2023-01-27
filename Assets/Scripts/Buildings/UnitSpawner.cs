6d38d18d9c515dccacb3d8a4bf6a87bb67e8223a
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-27 17:28:37 -0500

    Add unit spawning for players
    Currently just very basic spawning when clicking

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,36 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Buildings
+{
+    public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private GameObject unitPrefab = null;
+        [SerializeField] private Transform unitSpawnPoint = null;
+
+        #region Server
+
+        [Command]
+        private void CmdSpawnUnit()
+        {
+            var unitInstance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
+
+            NetworkServer.Spawn(unitInstance, connectionToClient);
+        }
+
+        #endregion
+
+        #region Client
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+            if (!isOwned) return;
+            
+            CmdSpawnUnit();
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,6 +5,17 @@ namespace Networking
 {
     public class RTSNetworkManager : NetworkManager
     {
-        
+        [SerializeField] private GameObject unitSpawnerPrefab;
+
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+            var addedPlayerTransform = conn.identity.transform;
+            var unitSpawnerInstance = Instantiate(unitSpawnerPrefab, addedPlayerTransform.position,
+                addedPlayerTransform.rotation);
+
+            NetworkServer.Spawn(unitSpawnerInstance, conn);
+        }
     }
 }
