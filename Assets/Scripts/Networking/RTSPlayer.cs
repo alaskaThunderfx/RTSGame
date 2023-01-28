11e17c26f4433e623525e536a4198f966e33abf4
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-27 22:53:18 -0500

    Add list storing for units client and server side

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using Mirror;
+using Units;
+using UnityEngine;
+
+namespace Networking
+{
+    public class RTSPlayer : NetworkBehaviour
+    {
+        [SerializeField] private List<Unit> _myUnits;
+
+        #region Server
+
+        public override void OnStartServer()
+        {
+            Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+            Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+        }
+
+        public override void OnStopServer()
+        {
+            Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+            Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+        }
+
+        private void ServerHandleUnitSpawned(Unit unit)
+        {
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) return;
+
+            _myUnits.Add(unit);
+        }
+
+        private void ServerHandleUnitDespawned(Unit unit)
+        {
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) return;
+
+            _myUnits.Remove(unit);
+        }
+
+        #endregion
+
+        #region Client
+
+        public override void OnStartClient()
+        {
+            if (!isClientOnly) return;
+
+            Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+            Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+        }
+
+        public override void OnStopClient()
+        {
+            if (!isClientOnly) return;
+
+            Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+            Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+        }
+
+        private void AuthorityHandleUnitSpawned(Unit unit)
+        {
+            if (!isOwned) return;
+            
+            _myUnits.Add(unit);
+        }
+
+        private void AuthorityHandleUnitDespawned(Unit unit)
+        {
+            if (!isOwned) return;
+            
+            _myUnits.Remove(unit);
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,7 +1,7 @@
+using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 namespace Units
 {
@@ -11,12 +11,45 @@ namespace Units
         [SerializeField] private UnityEvent onSelected = null;
         [SerializeField] private UnityEvent onDeselected = null;
 
+        public static event Action<Unit> ServerOnUnitSpawned; 
+        public static event Action<Unit> ServerOnUnitDespawned;
+        public static event Action<Unit> AuthorityOnUnitSpawned;
+        public static event Action<Unit> AuthorityOnUnitDespawned;
+
         public UnitMovement GetUnitMovement()
         {
             return unitMovement;
         }
+        
+        #region Server
+
+        public override void OnStartServer()
+        {
+            ServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopServer()
+        {
+            ServerOnUnitDespawned?.Invoke(this);
+        }
+
+        #endregion
 
         #region Client
+
+        public override void OnStartClient()
+        {
+            if (!isClientOnly || !isOwned) return;
+            
+            AuthorityOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopClient()
+        {
+            if (!isClientOnly || !isOwned) return;
+            
+            AuthorityOnUnitDespawned?.Invoke(this);
+        }
 
         [Client]
         public void Select()
